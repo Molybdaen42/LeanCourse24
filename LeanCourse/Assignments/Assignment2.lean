37c42a08066c1eb74c356051e17b4cd3523a0133
Nora Depenheuer <s6nodepe@uni-bonn.de>
2024-10-22 13:36:50 +0200

    assignment 2 done

--- LeanCourse/Assignments/Assignment2.lean
+++ LeanCourse/Assignments/Assignment2.lean
@@ -1,180 +1,43 @@
 import LeanCourse.Common
+import Init.Data.Queue
 import Mathlib.Data.Complex.Exponential
 open Real
-
-/-
-
-* From Mathematics in Lean https://leanprover-community.github.io/mathematics_in_lean
-  Read chapter 2, sections 2, 3, 4 and 5
-  Read chapter 3, sections 1, 4.
-
-* Do the exercises corresponding to these sections in the `LeanCourse/MIL` folder.
-  There are solutions in the solution folder in case you get stuck.
-
-* Hand in the solutions to the exercises below. Deadline: 22.10.2023.
-
-* Make sure the file you hand-in compiles without error.
-  Use `sorry` if you get stuck on an exercise.
--/
-
-/-! # Exercises to practice. -/
-
-example {a b : ℝ} (h1 : a + 2 * b = 4) (h2 : a - b = 1) : a = 2 := by {
-    have h: 4 = 1+3*b := by
-      calc
-        4 = a+2*b := by rw[←h1]
-        _ = a+(a-b)-(a-b)+2*b := by ring
-        _ = a+1-(a-b)+2*b := by rw[h2]
-        _ = 1+3*b := by ring
-    have h3: 1 = b := by
-      calc
-        1 = (4-1) / 3 := by ring
-        _ = (1+3*b-1) / 3 := by rw[← h]
-        _ = 3*b/3  := by ring
-        _ = b := by ring
-    rw[←h3] at h2
-    calc
-      a = a-1+1 := by ring
-      _ = 1+1 := by rw[h2]
-      _ = 2 := by ring
-  }
-
-example {u v : ℝ} (h1 : u + 1 = v) : u ^ 2 + 3 * u + 1 = v ^ 2 + v - 1 := by {
-  calc
-    u^2+3*u+1 = u^2+2*u+1+u+1-1 := by ring
-    _ = (u+1)^2+(u+1)-1 := by ring
-    _ = v^2+v-1 := by rw[h1]
-  }
-
-example (a b c x y : ℝ) (h : a ≤ b) (h2 : b < c) (h3 : x ≤ y) :
-    a + exp x ≤ c + exp (y + 2) := by {
-    apply add_le_add
-    · apply le_trans
-      · exact h
-      · exact le_of_lt h2
-    rw[exp_le_exp]
-    apply le_trans
-    · exact h3
-    · norm_num
-  }
-
-/-- Prove the following using `linarith`.
-Note that `linarith` cannot deal with implication or if and only if statements. -/
-example (a b c : ℝ) : a + b ≤ c ↔ a ≤ c - b := by {
-  constructor
-  · linarith
-  · linarith
-  }
-
-/- Note: for purely numerical problems, you can use `norm_num`
-(although `ring` or `linarith` also work in some cases). -/
-example : 2 + 3 * 4 + 5 ^ 6 ≤ 7 ^ 8 := by norm_num
-example (x : ℝ) : (1 + 1) * x + (7 ^ 2 - 35 + 1) = 2 * x + 15 := by norm_num
-
-/- You can prove many equalities and inequalities by being smart with calculations.
-In this case `linarith` can also prove this, but `calc` can be a lot more flexible. -/
-example {x y : ℝ} (hx : x + 3 ≤ 2) (hy : y + 2 * x ≥ 3) : y > 3 := by
-  calc
-    y = y + 2 * x - 2 * x := by ring
-    _ ≥ 3 - 2 * x := by gcongr
-    _ = 9 - 2 * (x + 3) := by ring
-    _ ≥ 9 - 2 * 2 := by gcongr
-    _ > 3 := by norm_num
-
-/-- It can be useful to add a `+ 0` in a calculational proof for `gcongr` -/
-example {m n : ℤ} : n ≤ n + m ^ 2 := by
-  -- gcongr doesn't make progress here
-  calc
-    n = n + 0 := by ring
-    _ ≤ n + m ^ 2 := by gcongr; exact sq_nonneg m
-
-/- Sometimes `congr`/`gcongr` goes too deep into a term.
-In that case, you can give `gcongr` a pattern with how deep it should enter the terms.
-When the pattern contains `?_`, it creates a subgoal with the corresponding terms
-on each side of the inequality.
-For `congr` you can also do this using the tactic `congrm`. -/
-example {a₁ a₂ b₁ b₂ c₁ c₂ : ℝ} (hab : a₁ + a₂ = b₁ + b₂) (hbc : b₁ + b₂ ≤ c₁ + c₂) :
-    a₁ + a₂ + 1 ≤ c₁ + c₂ + 1 := by
-  calc a₁ + a₂ + 1 = b₁ + b₂ + 1 := by congrm ?_ + 1; exact hab
-    _ ≤ c₁ + c₂ + 1 := by gcongr ?_ + 1 -- gcongr automatically applies `hbc`.
-
-
-example (x : ℝ) (hx : x = 3) : x ^ 2 + 3 * x - 5 = 13 := by
-  rw [hx]
-  norm_num
-
-example {m n : ℤ} : n - m ^ 2 ≤ n + 3 := by {
-  sorry
-  }
-
-example {a : ℝ} (h : ∀ b : ℝ, a ≥ -3 + 4 * b - b ^ 2) : a ≥ 1 := by {
-  sorry
-  }
-
-example {a₁ a₂ a₃ b₁ b₂ b₃ : ℝ} (h₁₂ : a₁ + a₂ + 1 ≤ b₁ + b₂) (h₃ : a₃ + 2 ≤ b₃) :
-  exp (a₁ + a₂) + a₃ + 1 ≤ exp (b₁ + b₂) + b₃ + 1 := by {
-  sorry
-  }
-
-
-/- Divisibility also gives an order. Warning: divisibility uses a unicode character,
-which can be written using `\|`. -/
-
-/-- Prove this using calc. -/
-lemma exercise_division (n m k l : ℕ) (h₁ : n ∣ m) (h₂ : m = k) (h₃ : k ∣ l) : n ∣ l := by {
-  sorry
-  }
-
-
-/- We can also work with abstract partial orders. -/
-
-section PartialOrder
-
-variable {X : Type*} [PartialOrder X]
-variable (x y z : X)
-
-/- A partial order is defined to be an order relation `≤` with the following axioms -/
-#check x ≤ y
-#check (le_refl x : x ≤ x)
-#check (le_trans : x ≤ y → y ≤ z → x ≤ z)
-#check (le_antisymm : x ≤ y → y ≤ x → x = y)
-
-/- every preorder comes automatically with an associated strict order -/
-example : x < y ↔ x ≤ y ∧ x ≠ y := lt_iff_le_and_ne
-
-/- the reverse order `≥`/`>` is defined from `≤`/`<`.
-In Mathlib we usually state lemmas using `≤`/`<` instead of `≥`/`>`. -/
-example : x ≥ y ↔ y ≤ x := by rfl
-example : x > y ↔ y < x := by rfl
-
-
-example (hxy : x ≤ y) (hyz : y ≤ z) (hzx : z ≤ x) : x = y ∧ y = z ∧ x = z := by {
-  sorry
-  }
-
-
-end PartialOrder
-
 
 /-! # Exercises to hand-in. -/
 
 /- Prove this using a calculation. -/
 lemma exercise_calc_real {t : ℝ} (ht : t ≥ 10) : t ^ 2 - 3 * t - 17 ≥ 5 := by {
-  sorry
+  have h1 : t-6 ≥ 0 := by linarith
+  have h2 : t+2 ≥ 0 := by linarith
+  have h3 : (t-6)*(t+2) ≥ 0 := by exact mul_nonneg h1 h2
+  calc
+    t^2 - 3*t - 17 = t^2 - 4*t - 12 + t - 5 := by ring
+                 _ = (t-6)*(t+2) + t - 5    := by ring
+                 _ ≥ (t-6)*(t+2) + 5        := by linarith
+                 _ ≥ 5                      := by linarith
   }
 
 /- Prove this using a calculation.
-The arguments `{R : Type*} [CommRing R] {t : R}` mean
-"let `t` be an element of an arbitrary commutative ring `R`." -/
+The arguments {R : Type*} [CommRing R] {t : R} mean
+"let t be an element of an arbitrary commutative ring R." -/
 lemma exercise_calc_ring {R : Type*} [CommRing R] {t : R}
     (ht : t ^ 2 - 4 = 0) :
     t ^ 4 + 3 * t ^ 3 - 3 * t ^ 2 - 2 * t - 2 = 10 * t + 2 := by {
-  sorry
+  calc
+    t^4 + 3*t^3 - 3*t^2 - 2*t - 2 = (t^2-4)*(t^2 + 3*t + 1) + 10*t+2 := by ring
+                                _ = 0*(t^2+3*t+1) + 10*t+2           := by rw[ht]
+                                _ = 10*t+2                           := by ring
   }
 
 /-- Prove this using a calculation. -/
 lemma exercise_square {m n k : ℤ} (h : m ^ 2 + n ≤ 2) : n + 1 ≤ 3 + k ^ 2 := by {
-  sorry
+  calc
+    n + 1 ≤ 3 + n - 2         := by linarith
+        _ ≤ 3 + n - (m^2 + n) := by linarith
+        _ ≤ 3 - m^2           := by linarith
+        _ ≤ 3 - 0             := by gcongr; exact sq_nonneg m
+        _ ≤ 3 + 0             := by linarith
+        _ ≤ 3 + k^2           := by gcongr; exact sq_nonneg k
   }
 
 
@@ -182,35 +45,81 @@ lemma exercise_square {m n k : ℤ} (h : m ^ 2 + n ≤ 2) : n + 1 ≤ 3 + k ^ 2 
 section Min
 variable (a b c : ℝ)
 
-/- The following theorems characterize `min`.
-Let's this characterization it to prove that `min` is commutative and associative.
-Don't use other lemmas about `min` from Mathlib! -/
+/- The following theorems characterize min.
+Let's this characterization it to prove that min is commutative and associative.
+Don't use other lemmas about min from Mathlib! -/
 #check (min : ℝ → ℝ → ℝ)
 #check (min_le_left a b : min a b ≤ a)
 #check (min_le_right a b : min a b ≤ b)
 #check (le_min : c ≤ a → c ≤ b → c ≤ min a b)
 
 lemma exercise_min_comm : min a b = min b a := by {
-  sorry
+  have h11 : min a b ≤ b := by exact min_le_right a b
+  have h12 : min a b ≤ a := by exact min_le_left a b
+  have h1 : min a b ≤ min b a := by exact le_min h11 h12
+  have h21 : min b a ≤ a := by exact min_le_right b a
+  have h22 : min b a ≤ b := by exact min_le_left b a
+  have h2 : min a b ≥ min b a := by exact le_min h21 h22
+  apply le_antisymm h1 h2
   }
 
 lemma exercise_min_assoc : min a (min b c) = min (min a b) c := by {
-  sorry
+  apply le_antisymm
+  apply le_min
+  apply le_min
+  apply min_le_left
+  calc
+    min a (min b c) ≤ min b c := by exact min_le_right a (min b c)
+                  _ ≤ b       := by exact min_le_left b c
+  calc
+    min a (min b c) ≤ min b c := by exact min_le_right a (min b c)
+                  _ ≤ c       := by exact min_le_right b c
+  apply le_min
+  calc
+    min (min a b) c ≤ min a b := by exact min_le_left (min a b) c
+                  _ ≤ a       := by exact min_le_left a b
+  apply le_min
+  calc
+    min (min a b) c ≤ min a b := by exact min_le_left (min a b) c
+                  _ ≤ b       := by exact min_le_right a b
+  exact min_le_right (min a b) c
   }
 
 end Min
 
 /- Prove that the following function is continuous.
-You can use `Continuous.div` as the first step,
+You can use Continuous.div as the first step,
 and use the search techniques to find other relevant lemmas.
-`ne_of_lt`/`ne_of_gt` will be useful to prove the inequality. -/
+ne_of_lt/ne_of_gt will be useful to prove the inequality. -/
 lemma exercise_continuity : Continuous (fun x ↦ (sin (x ^ 5) * cos x) / (x ^ 2 + 1)) := by {
-  sorry
+  apply Continuous.div
+  apply Continuous.mul
+  refine Continuous.comp' ?hf.hf.hg ?hf.hf.hf
+  · exact continuous_sin
+  · exact continuous_pow 5
+  · exact continuous_cos
+  apply Continuous.add
+  apply Continuous.pow
+  exact continuous_id
+  exact continuous_const
+  have h1 (x:ℝ ): x^2+1>0 := by{
+    apply add_pos_of_nonneg_of_pos
+    · apply pow_two_nonneg
+    · linarith
   }
+  have h2 (x:ℝ): x^2+1≠ 0 := by{
+    apply ne_of_gt
+    apply h1
+  }
+  exact h2
+}
 
-/- Prove this only using `intro`/`constructor`/`obtain`/`exact` -/
+/- Prove this only using intro/constructor/obtain/exact -/
 lemma exercise_and_comm : ∀ (p q : Prop), p ∧ q ↔ q ∧ p := by {
-  sorry
+  intro (p : Prop) (q : Prop)
+  constructor
+  exact And.symm
+  exact And.symm
   }
 
 
@@ -219,18 +128,26 @@ def Nondecreasing (f : ℝ → ℝ) : Prop := ∀ x₁ x₂ : ℝ, x₁ ≤ x₂
 
 lemma exercise_nondecreasing_comp (f g : ℝ → ℝ) (hg : Nondecreasing g) (hf : Nondecreasing f) :
     Nondecreasing (g ∘ f) := by {
-  sorry
+  unfold Nondecreasing
+  unfold Nondecreasing at hg hf
+  intro (x1 : ℝ) (x2 : ℝ) (hx : x1 ≤ x2)
+  obtain hfx := hf x1 x2 (by assumption)
+  exact hg (f x1) (f x2) (by assumption)
   }
 
 
-/-- Note: `f + g` is the function defined by `(f + g)(x) := f(x) + g(x)`.
-  `simp` can simplify `(f + g) x` to `f x + g x`. -/
+/-- Note: f + g is the function defined by (f + g)(x) := f(x) + g(x).
+  simp can simplify (f + g) x to f x + g x. -/
 lemma exercise_nondecreasing_add (f g : ℝ → ℝ) (hf : Nondecreasing f) (hg : Nondecreasing g) :
     Nondecreasing (f + g) := by {
-  sorry
+  unfold Nondecreasing
+  unfold Nondecreasing at hf hg
+  intro (x1 : ℝ) (x2 : ℝ) (hx : x1 ≤ x2)
+  obtain hfx := hf x1 x2 (by assumption)
+  obtain hgx := hg x1 x2 (by assumption)
+  simp
+  linarith
   }
-
-
 
 /-- Prove the following property of even. -/
 def EvenFunction (f : ℝ → ℝ) : Prop :=
@@ -238,5 +155,20 @@ def EvenFunction (f : ℝ → ℝ) : Prop :=
 
 lemma exercise_even_iff (f g : ℝ → ℝ) (hf : EvenFunction f) :
     EvenFunction (f + g) ↔ EvenFunction g := by {
-  sorry
+  unfold EvenFunction
+  unfold EvenFunction at hf
+  simp
+  constructor
+
+  intro h1
+  intro (x : ℝ)
+  calc
+    g x = f x + g x - f x          := by ring
+      _ = f (-x) + g (-x) - f x    := by rw[h1 x]
+      _ = f (-x) + g (-x) - f (-x) := by rw[hf x]
+      _ = g (-x)                   := by ring
+
+  intro h2
+  intro (x : ℝ)
+  rw[h2,hf]
   }

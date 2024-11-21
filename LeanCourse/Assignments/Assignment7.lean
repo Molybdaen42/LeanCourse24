68154e98433d7e62fb9b6c1954b2f19f50a0ef8f
Joachim Roscher <99897783+Molybdaen42@users.noreply.github.com>
2024-11-21 18:51:06 +0100

    bleep

--- LeanCourse/Assignments/Assignment7.lean
+++ LeanCourse/Assignments/Assignment7.lean
@@ -153,26 +153,33 @@ lemma add_pow_eq_pow_add_pow (x y : R) : (x + y) ^ p = x ^ p + y ^ p := by {
     | succ n hn => simp [range_add_one]
   have dvd_choose : ∀ i ∈ Ioo 0 p, p ∣ Nat.choose p i := by
     intro i i_in_Ioo
-    -- Apply the suggested lemma
-    apply (CharP.cast_eq_zero_iff R p (Nat.choose p i)).mp
-    sorry
+    -- since i ∈ Ioo 0 p, it is > 0 (but we'll just need ≠ 0) and < p
+    obtain ⟨zero_lt_i,i_lt_p⟩ := mem_Ioo.mp i_in_Ioo
+    -- from this follows that p | Nat.choose p i
+    apply Prime.dvd_choose_self hp' (Nat.ne_of_lt zero_lt_i).symm i_lt_p
   have h6 : ∑ i in Ioo 0 p, x ^ i * y ^ (p - i) * Nat.choose p i = 0 :=
   calc
     _ =  ∑ i in Ioo 0 p, x ^ i * y ^ (p - i) * 0 := by
-      have : ∀ i ∈ Ioo 0 p, ((Nat.choose p i) : R) = 0 ↔ p ∣ (Nat.choose p i) := by
+      have h1 : ∀ i ∈ Ioo 0 p, ((Nat.choose p i) : R) = 0 ↔ p ∣ (Nat.choose p i) := by
         intro i hi
         exact (CharP.cast_eq_zero_iff R p (Nat.choose p i))
-      have : ∀ i ∈ Ioo 0 p, ((Nat.choose p i) : R) = 0 := by
+      have h2 : ∀ i ∈ Ioo 0 p, ((Nat.choose p i) : R) = 0 := by
         intro i hi
-        apply (this i hi).mpr
+        apply (h1 i hi).mpr
         exact dvd_choose i hi
 
       --apply sum_eq_sum_iff_of_le
       --ext i
       sorry
-    _ = 0 := by sorry
+    _ = 0 := by simp
+  -- Use the recommended lemma.
+  rw [add_pow]
+  -- take the p+1-th term out of the sum (maybe per Finset.sum_eq_add_sum_diff_singleton?)
+  -- rw [range_eq_insert_Ioo]
+  -- take the 0-th term out of the sum
   sorry
   }
+#check Finset.sum_eq_add_sum_diff_singleton
 #check CharP.cast_eq_zero_iff
 #check Finset.sum_equiv
 #check Finset.sum_eq_sum_iff_of_le

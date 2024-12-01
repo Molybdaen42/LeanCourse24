accb3b883cf4ef8819a863070a83852088963e38
Nora Depenheuer <97042192+N-DPH@users.noreply.github.com>
2024-12-01 21:22:58 +0100

    Versuch lean wieder ans Laufen zu kriegen

--- LeanCourse/MIL/C01_Introduction/solutions/Solutions_S02_Overview.lean
+++
@@ -1,5 +0,0 @@
-import LeanCourse.Common
-
-open Nat
-
--- There are no exercises in this section.
--- LeanCourse/MIL/C09_Linear_Algebra/solutions/Solutions_S01_Vector_Spaces.lean
+++
@@ -1,5 +0,0 @@
-import Mathlib.LinearAlgebra.Matrix.Determinant.Basic
-import Mathlib.LinearAlgebra.Eigenspace.Minpoly
-import Mathlib.LinearAlgebra.Charpoly.Basic
-
-import LeanCourse.Common
